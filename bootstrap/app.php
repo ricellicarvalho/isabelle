89351ec6f0190626d188ae2dcb10ba648b4dccfd
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2026-04-08 03:00:39 -0300

    Ajustando docker-compose para produção

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // App rodando atrás do reverse proxy do host (nginx → container nginx).
+        // Confia em qualquer proxy e respeita os cabeçalhos X-Forwarded-* para
+        // que url()/route()/redirect()/Filament gerem URLs com https corretas.
+        $middleware->trustProxies(at: '*', headers:
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
